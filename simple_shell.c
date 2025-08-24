a1b9cca9669070e606b70da6b8783e845d1ad719
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-08-24 11:51:53 +0000

    Add simple shell basic implementation

--- simple_shell.c
+++ simple_shell.c
@@ -1,71 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
-
-extern char **environ;
 
 #define MAX_INPUT 1024
 
+extern char **environ;
+
 int main(void)
 {
-    char input[MAX_INPUT];
-    pid_t pid;
-    int status;
-    size_t len;
-    char *argv[2];
+	char input[MAX_INPUT];
+	pid_t pid;
+	int status;
+	char *argv[2];
+	size_t len;
 
-    while (1)
-    {
-        /* Yalnız terminaldan daxil olduqda prompt göstər */
-        if (isatty(STDIN_FILENO))
-        {
-            printf("$ ");
-            fflush(stdout);
-        }
+	while (1)
+	{
+		/* Yalnız interaktiv rejimdə prompt göstər */
+		if (isatty(STDIN_FILENO))
+		{
+			printf("$ ");
+			fflush(stdout);
+		}
 
-        if (fgets(input, MAX_INPUT, stdin) == NULL)
-        {
-            /* Ctrl+D basıldıqda çıxış */
-            if (isatty(STDIN_FILENO))
-                printf("\n");
-            break;
-        }
+		/* İstifadəçi girişini oxu */
+		if (fgets(input, MAX_INPUT, stdin) == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			break;
+		}
 
-        /* Yeni sətri sil */
-        len = strlen(input);
-        if (len > 0 && input[len - 1] == '\n')
-            input[len - 1] = '\0';
+		/* Yeni sətrin (\n) silinməsi */
+		len = strlen(input);
+		if (len > 0 && input[len - 1] == '\n')
+			input[len - 1] = '\0';
 
-        if (input[0] == '\0')  /* boş sətir, keç */
-            continue;
+		/* Boş daxilolmanı keç */
+		if (input[0] == '\0')
+			continue;
 
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			argv[0] = input;
+			argv[1] = NULL;
 
-        if (pid == 0)
-        {
-            /* Child prosesi: execve ilə işlət */
-            argv[0] = input;
-            argv[1] = NULL;
+			if (execve(argv[0], argv, environ) == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			waitpid(pid, &status, 0);
+		}
+	}
 
-            if (execve(input, argv, environ) == -1)
-            {
-                perror("Error");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            /* Parent: child-ı gözləyir */
-            waitpid(pid, &status, 0);
-        }
-    }
-
-    return 0;
+	return 0;
 }
