4a6729305a929760f03ba7226809996c1b05ea8c
AisunMagaramova <maharramovaaysun1122@gmail.com>
2025-08-24 13:29:46 +0000

    t 2

--- simple_shell.c
+++ simple_shell.c
@@ -4,60 +4,47 @@
 #include <string.h>
 #include <sys/wait.h>
 
+extern char **environ;
+
 #define MAX_INPUT 1024
 
-/**
- * main - Entry point for the simple shell
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	char input[MAX_INPUT];
-	char *argv[2];
-	pid_t pid;
-	int status;
-	size_t len;
+    char input[MAX_INPUT];
+    char *argv[2];
+    pid_t pid;
+    int status;
+    size_t len;
 
-	while (1)
-	{
-		if (fgets(input, MAX_INPUT, stdin) == NULL)
-		{
-			/* Handle Ctrl+D (EOF) */
-			write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
+    while (1)
+    {
 
-		/* Remove newline character */
-		len = strlen(input);
-		if (len > 0 && input[len - 1] == '\n')
-			input[len - 1] = '\0';
+        if (fgets(input, MAX_INPUT, stdin) == NULL)
+            break;
 
-		if (input[0] == '\0')
-			continue;
+        len = strlen(input);
+        if (len > 0 && input[len - 1] == '\n')
+            input[len - 1] = '\0';
 
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			argv[0] = input;
-			argv[1] = NULL;
+        argv[0] = input;
+        argv[1] = NULL;
 
-			if (execve(argv[0], argv, NULL) == -1)
-			{
-				perror("execve");
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			waitpid(pid, &status, 0);
-		}
-	}
-
-	return (0);
+        pid = fork();
+        if (pid == 0)
+        {
+            execve(argv[0], argv, environ);
+            perror("execve");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            waitpid(pid, &status, 0);
+        }
+    }
+    return 0;
 }
